11d31ac3c83832929b951ea95f01f9ea5319ef62
Carmen Renee <carmengenpt@gmail.com>
2019-03-03 19:53:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ def items
 end 
 
 def void_last_transaction
-  self.total -= self.last_transaction[1] * self.last_transaction[2]
+  self.total -= self.last_transaction[1] * self
     self.last_transaction[2].times do
       self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count).
     end 
